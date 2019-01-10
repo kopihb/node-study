2f3523cc1070ddfd03875232bba9b4587132fcee
Serhiy Korynevych <>
2019-01-10 18:02:20 +0200

    added express

--- app.js
+++ app.js
@@ -1,44 +1,13 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-const server =  http.createServer((req, res) => {
+app.use(bodyParser.urlencoded({extended: false}));
 
-    const url = req.url;
-    const method = req.method;
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title> Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const  body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk)
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
-        })
-
-
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title> My first Node page</title></head>');
-    res.write('<body><h1>My first Node.js SERVER</h1></body>');
-    res.write('</html>');
-    res.end();
-});
-
-server.listen(3000);
+app.listen(3001);
