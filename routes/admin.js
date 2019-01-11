08345f39046a9defed4a0defbbe2233d83e3335a
Serhiy Korynevych <>
2019-01-11 16:49:37 +0200

    added added html + routes + css

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,18 @@
-const  express =require('express');
+const path = require('path');
+
+const express =require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.use('/add-product', (req, res, next) => {
+router.get('/add-product', (req, res, next) => {
     console.log('In another middleware!');
-    res.send(
-        '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button> </form>');
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
