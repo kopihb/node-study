143c28be2d953b1ce43830f221573bbf9960611c
Serhiy Korynevych <>
2019-01-14 11:43:48 +0200

    navigation

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,6 @@ const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 
 });
