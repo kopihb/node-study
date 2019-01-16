7d69afa9073d3d79f5e0fd9dffdfb6edbcb5074a
Serhiy Korynevych <>
2019-01-16 19:47:55 +0200

    improve project routes and models done

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
-// /admin/add-product => GET
+// /admin/products => GET
 router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
@@ -17,6 +17,8 @@ router.post('/add-product', adminController.postAddProduct);
 
 router.get('/edit-product/:productId', adminController.getEditProduct);
 
-router.post('/edit-product');
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
