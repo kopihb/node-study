0bfd168cb911a982081fd6e99b981bd1ceda8409
Serhiy Korynevych <>
2019-01-21 12:30:19 +0200

    improve project sql

--- controllers/admin.js
+++ controllers/admin.js
@@ -54,8 +54,13 @@ exports.postAddProduct = (req, res, next) => {
    const description = req.body.description;
 
     const product = new Product(null, title, imageUrl, description, price);
-    product.save()
-    res.redirect('/');
+    product
+        .save()
+        .then(() => {
+            res.redirect('/');
+        })
+        .catch(err =>  console.log(err));
+
 };
 
 exports.getProducts = (req, res, next) => {
